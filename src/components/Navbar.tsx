6262b73ce8ddcf26160ae747619a2730f357eb6b
Quincy Thai <quincythai50@icloud.com>
2024-04-20 10:11:18 -0700

    Refine navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,39 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="bg-white shadow-sm">
       {/* Prevent from stretching too much */}
-      <div className="max-w-6xl mx-auto px-4">
+      <div className="max-w-6xl mx-auto px-6 py-4">
         <div className="flex justify-between items-center">
           {/* Logo */}
-          <Link to="/" className="flex items-center py-4 px-2">
-            <img src="/logo.png" alt="Brand" className="h-8 w-auto" />
+          <Link to="/" className="flex items-center">
+            <img src="/logo.png" alt="Brand" className="h-10 w-auto" />
           </Link>
 
           {/* Links */}
-          <div className="flex items-center space-x-4 ml-auto">
+          <div className="flex items-center space-x-4 ml-auto gap-4">
             <Link
               to="/"
-              className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+              className=" text-gray-500 font-semibold hover:text-green-500 transition duration-300"
             >
               Home
             </Link>
             <Link
               to="/about"
-              className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+              className=" text-gray-500 font-semibold hover:text-green-500 transition duration-300"
             >
               About
             </Link>
             <Link
               to="/projects"
-              className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+              className=" text-gray-500 font-semibold hover:text-green-500 transition duration-300"
             >
               Projects
             </Link>
             <Link
               to="/contact"
-              className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+              className=" text-gray-500 font-semibold hover:text-green-500 transition duration-300"
             >
               Contact
             </Link>
